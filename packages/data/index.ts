12d9f11340081c71ef70862f6608333b76e15e7b
Felipe Bruce <felipenxk8@gmail.com>
2021-09-08 17:22:57 -0300

    Changing data folder structure

--- packages/data/index.ts
+++ packages/data/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {User} from "./entities/User";
 
 createConnection().then(async connection => {
 
